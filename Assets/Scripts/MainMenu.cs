94a3d73165f6747a46fbd6b48289077056849c7a
evilgamer163 <evilgamestm@gmail.com>
2019-02-21 12:16:53 +0400

    FirstBuild

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,12 +8,44 @@ public class MainMenu : MonoBehaviour {
 
     [SerializeField] Image image;
     [SerializeField] Sprite[] sprites;
+    [SerializeField] GameObject howToPlayPanel;
+    [SerializeField] Text howToPlayButtonText;
 
     bool soundController = true;
+
+    void Start()
+    {
+        if (AudioListener.volume == 0)
+        {
+            soundController = false;
+            image.sprite = sprites[0];
+        }
+        else
+        {
+            soundController = true;
+            image.sprite = sprites[1];
+        }
+    }
+
+    public void HowToPlayPanel()
+    {
+        if (howToPlayPanel.activeSelf)
+        {
+            howToPlayPanel.SetActive(false);
+            howToPlayButtonText.text = " HOW TO \nPLAY?";
+
+        }
+        else
+        {
+            howToPlayPanel.SetActive(true);
+            howToPlayButtonText.text = "BACK";
+        }
+    }
 
     public void LoadScene(int i)
     {
         SceneManager.LoadScene(i);
+        PlayerPrefs.GetFloat("volume", AudioListener.volume);
     }
 
     public void SoundButton()
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] Image image;
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject button;
+
+    bool soundController = true;
+
+    void Start () {
+        if (AudioListener.volume == 0)
+        {
+            soundController = false;
+            image.sprite = sprites[0];
+        }
+        else
+        {
+            soundController = true;
+            image.sprite = sprites[1];
+        }
+    }
+
+    public void PauseButton()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0.0000000001f;
+            pausePanel.SetActive(true);
+            button.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+            button.SetActive(true);
+        }
+    }
+
+    public void SoundButton()
+    {
+        if (soundController == true)
+        {
+            AudioListener.volume = 0;
+            soundController = false;
+            image.sprite = sprites[0];
+
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            soundController = true;
+            image.sprite = sprites[1];
+        }
+    }
+}
